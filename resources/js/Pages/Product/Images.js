7d846cfa0c5d0d97bf3184319c17d91f48ea91de
Abel <abel291m@gmail.com>
2022-02-18 04:00:16 -0500

    carousel product detail

--- resources/js/Pages/Product/Images.js
+++ resources/js/Pages/Product/Images.js
@@ -1,31 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { FreeMode, Navigation, Thumbs } from "swiper";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
 
 const Images = ({ product }) => {
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
     return (
-        <div className="flex items-stretch gap-x-4 ">
-            <div className="w-1/5">
-                <div className="space-y-4">
+        <div className="">
+            <div className="relative mb-3">
+                <button className="swiper-button-next   ">
+                    <ChevronRightIcon className="h-full w-full bg-white text-gray-700 shadow-sm  " />
+                </button>
+                <button className="swiper-button-prev  ">
+                    <ChevronLeftIcon className="h-full w-full bg-white text-gray-700 shadow-sm  " />
+                </button>
+                <Swiper
+                    centeredSlides={true}
+                    thumbs={{ swiper: thumbsSwiper }}
+                    modules={[FreeMode, Navigation, Thumbs]}
+                    className="mySwiper2 h-[500px] bg-orange-50"
+                    navigation={{
+                        nextEl: ".swiper-button-next",
+                        prevEl: ".swiper-button-prev",
+                    }}
+                >
                     {product.images.map((item, key) => (
-                        <div key={key}>
+                        <SwiperSlide key={key}>
                             <img
                                 src={item.img}
                                 alt={item.alt}
                                 alt={item.title}
-                                className="w-full object-cover"
+                                className="mx-auto max-h-full max-w-full"
                             />
-                        </div>
+                        </SwiperSlide>
                     ))}
-                </div>
+                </Swiper>
             </div>
-            <div className="flex  w-4/5 items-center bg-orange-50">
-                <img
-                    src={product.img}
-                    alt={product.name}
-                    alt={product.name}
-                    className=" max-h-full max-w-full "
-                />
+            <div>
+                <Swiper
+                    onSwiper={setThumbsSwiper}
+                    spaceBetween={12}
+                    slidesPerView={4}
+                    freeMode={true}
+                    watchSlidesProgress={true}
+                    modules={[FreeMode, Navigation, Thumbs]}
+                >
+                    {product.images.map((item, key) => (
+                        <SwiperSlide key={key} className="h-28 cursor-pointer">
+                            <img
+                                src={item.img}
+                                alt={item.alt}
+                                alt={item.title}
+                                className=" w-full object-cover"
+                            />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </div>
         </div>
+        // <div className="flex items-stretch gap-x-4 ">
+        //     <div className="w-1/5">
+        //         <div className="space-y-4">
+        //             {product.images.map((item, key) => (
+        //                 <div key={key}>
+        //                     <img
+        //                         src={item.img}
+        //                         alt={item.alt}
+        //                         alt={item.title}
+        //                         className="w-full object-cover"
+        //                     />
+        //                 </div>
+        //             ))}
+        //         </div>
+        //     </div>
+        //     <div className="flex  w-4/5 items-center bg-orange-50">
+        //         <img
+        //             src={product.img}
+        //             alt={product.name}
+        //             alt={product.name}
+        //             className=" max-h-full max-w-full "
+        //         />
+        //     </div>
+        // </div>
     );
 };
 
